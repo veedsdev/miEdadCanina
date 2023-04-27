857bb3986d1f7089ca43e93b855fc3168b2c2856
EDGAR VEGA ARAGON <ingvegaedgar@gmail.com>
2023-04-27 17:01:27 -0600

    Toast message and Logs

--- app/src/main/java/mx/com/miedadcanina/MainActivity.kt
+++ app/src/main/java/mx/com/miedadcanina/MainActivity.kt
@@ -2,9 +2,11 @@ package mx.com.miedadcanina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,11 +18,25 @@ class MainActivity : AppCompatActivity() {
         val tvResultAge = findViewById<TextView>(R.id.tv_result_age)
         val btnCalculate = findViewById<Button>(R.id.btn_calculate)
 
+//        Log.v("MainActivity", "MainActivity is created verbose")
+//        Log.d("MainActivity", "MainActivity is created debug")
+//        Log.i("MainActivity", "MainActivity is created info")
+//        Log.w("MainActivity", "MainActivity is created warning")
+//        Log.e("MainActivity", "MainActivity is created error")
+
+
         btnCalculate.setOnClickListener{
             val ageString = etAge.text.toString()
-            val ageInt = ageString.toInt()
-            val result = ageInt * 7
-            tvResultAge.text="Tu edad canina es de $result años"
+            if(ageString.isNotEmpty()){
+                val ageInt = ageString.toInt()
+                val result = ageInt * 7
+                tvResultAge.text="Tu edad canina es de $result años"
+            }
+            else{
+
+                Log.d("MainActivity", "ageString is empty")
+                Toast.makeText(this, "Debes insertar tu edad", Toast.LENGTH_SHORT).show()
+            }
         }
 
     }
